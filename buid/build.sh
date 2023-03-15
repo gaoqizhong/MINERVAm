1c6118386672bb88c90378232b3b3480bf47f10d
gaoqizhong <gaoqizhong@csu.edu.cn>
2023-03-15 16:16:11 +0800

    add build

--- buid/build.sh
+++ buid/build.sh
@@ -1,8 +1,4 @@
-### build
-
-```shell
 sudo -i
-git clone 
 
 # install Anaconda3
 wget -P /tmp https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
@@ -10,11 +6,8 @@ bash /tmp/Anaconda3-2020.02-Linux-x86_64.sh
 source ~/.bashrc
 
 # install NVIDIA dirver
-sudo gedit /etc/modprobe.d/blacklist.conf
-
-
+cp /home/featurize/myMINERVA/build/blacklist.conf /etc/modprobe.d
+update-initramfs -u
 
 # install CUDA
 wget https://developer.download.nvidia.com/compute/cuda/11.2.0/local_installers/cuda_11.2.0_460.27.04_linux.run
-```
-
